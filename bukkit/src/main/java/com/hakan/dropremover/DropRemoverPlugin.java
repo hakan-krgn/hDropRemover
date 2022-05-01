daeb973f3e4f2a072f1d048588da0587a40634c3
Hakan Kargın <hakankargin03@gmail.com>
2022-05-01 22:27:45 +0300

    Initialize hCore

--- bukkit/src/main/java/com/hakan/dropremover/DropRemoverPlugin.java
+++ bukkit/src/main/java/com/hakan/dropremover/DropRemoverPlugin.java
@@ -1,5 +1,6 @@
 package com.hakan.dropremover;
 
+import com.hakan.core.HCore;
 import com.hakan.dropremover.hooks.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -7,6 +8,7 @@ public class DropRemoverPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        HCore.initialize(this);
         Metrics.initialize(this);
         DropRemoverHandler.initialize(this);
     }
